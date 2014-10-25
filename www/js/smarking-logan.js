dc39db138bacdc9ecdf884ab2dc4e2f2da528d18
Hao Zhou <royhzhou@gmail.com>
2014-10-25 16:02:47 -0400

    add simple auth module

--- www/js/smarking-logan.js
+++ www/js/smarking-logan.js
@@ -2,10 +2,15 @@ var myApp = new Framework7({
     modalTitle: 'Logan Admin Portal',
     animateNavBackIcon: true
 });
-
+$.ajaxSetup({
+    data: {
+        "token": $.jStorage.get("token"),
+    },
+    type: "POST"
+});
 // Expose Internal DOM library
 var $$ = Framework7.$;
-
+var BASE_URL = 'http://127.0.0.1:8080/';
 // Add main view
 var mainView = myApp.addView('.view-main', {
     // Enable Dynamic Navbar for this view
@@ -16,6 +21,10 @@ var rightView = myApp.addView('.view-right', {
     // Enable Dynamic Navbar for this view
     dynamicNavbar: true
 });
+if (!$.jStorage.get("token")) {
+    console.log("I am here");
+    mainView.loadPage('login.html');
+}
 // Show/hide preloader for remote ajax loaded pages
 // Probably should be removed on a production/local app
 $$(document).on('ajaxStart', function() {
@@ -32,89 +41,89 @@ $$(document).on('pageInit', function(e) {
     remoteURL = '';
     type = 'occupancy';
     isPrediction = 0;
-    fromdate = myApp.getDate(-30);   // Chart data from the first day of current year
-    todate = myApp.getDate();       // Chart data to today
+    fromdate = myApp.getDate(-30); // Chart data from the first day of current year
+    todate = myApp.getDate(); // Chart data to today
 
+    if (page.name === 'login') {
+        $$('.loginBtn').on('click', function() {
+            var name = $('#name').val();
+            var password = $('#password').val();
+            myApp.login(name, password);
+        });
+    }
     if (page.name === 'centralWest') {
-        remoteURL = 'http://smarking.net/service/garage?garage=centralWest&';
+        remoteURL = BASE_URL + 'garage?garage=centralWest&';
     } else if (page.name === 'terminalB') {
-        remoteURL = 'http://smarking.net/service/garage?garage=terminalB&';
+        remoteURL = BASE_URL + 'garage?garage=terminalB&';
     } else if (page.name === 'terminalE') {
-        remoteURL = 'http://smarking.net/service/garage?garage=terminalE&';
+        remoteURL = BASE_URL + 'garage?garage=terminalE&';
     } else if (page.name === 'economyLot') {
-        remoteURL = 'http://smarking.net/service/garage?garage=economyLot&';
+        remoteURL = BASE_URL + 'garage?garage=economyLot&';
     }
     myApp.loadChart();
-    
     if (page.name === 'centralWest' || page.name === 'terminalB' || page.name === 'terminalE' || page.name === 'economyLot') {
-        
-        $$('.action-period').on('click', function () {
+        $$('.action-period').on('click', function() {
             myApp.updatePeriod();
-            var buttons = [
-                {
-                    fromdate: true
-                }, {
-                    todate: true
-                }, {
-                    text: 'Get Chart',
-                    onClick: function () {
-                        fromdate = $('.from-date').val();
-                        todate = $('.to-date').val();
-                        myApp.loadChart(fromdate, todate);
-                    }
-                },
-            ];
+            var buttons = [{
+                fromdate: true
+            }, {
+                todate: true
+            }, {
+                text: 'Get Chart',
+                onClick: function() {
+                    fromdate = $('.from-date').val();
+                    todate = $('.to-date').val();
+                    myApp.loadChart(fromdate, todate);
+                }
+            }, ];
             myApp.myactions(buttons);
         });
-        
-        $$('.action-type').on('click', function () {
+        $$('.action-type').on('click', function() {
             myApp.updatePeriod();
-            var buttons = [
-                {
-                    text: 'Occupancy',
-                    onClick: function () {
-                        type = 'occupancy';
-                        myApp.loadChart();
-                    }
-                }, {
-                    text: 'Entry',
-                    onClick: function () {
-                        type = 'entry';
-                        myApp.loadChart();
-                    }
-                }, {
-                    text: 'Exits',
-                    onClick: function () {
-                        type = 'exits';
-                        myApp.loadChart();
-                    }
-                }, {
-                    text: 'Duration',
-                    onClick: function () {
-                        type = 'duration';
-                        myApp.loadChart();
-                    }
-                },
-            ];
+            var buttons = [{
+                text: 'Occupancy',
+                onClick: function() {
+                    type = 'occupancy';
+                    myApp.loadChart();
+                }
+            }, {
+                text: 'Entry',
+                onClick: function() {
+                    type = 'entry';
+                    myApp.loadChart();
+                }
+            }, {
+                text: 'Exits',
+                onClick: function() {
+                    type = 'exits';
+                    myApp.loadChart();
+                }
+            }, {
+                text: 'Duration',
+                onClick: function() {
+                    type = 'duration';
+                    myApp.loadChart();
+                }
+            }, ];
             myApp.myactions(buttons);
-        });        
-        
+        });
         $$('.history-selected').on('click', function() {
             myApp.updatePeriod();
             isPrediction = 0;
             myApp.loadChart();
             myApp.closePanel();
         });
-
         $$('.predicton-selected').on('click', function() {
             myApp.updatePeriod();
             isPrediction = 1;
             myApp.loadChart();
             myApp.closePanel();
         });
+        $$('.logout-selected').on('click', function() {
+            myApp.logout();
+        });
     }
 });
-
 $$('.pre-loader').on('click', function() {
     myApp.showPreloader();
 })
@@ -122,7 +131,6 @@ $$('.pre-loader').on('click', function() {
 $$('.popover a').on('click', function() {
     myApp.closeModal('.popover');
 });
-
 // Change statusbar bg when panel opened/closed
 $$('.panel-left').on('open', function() {
     $$('.statusbar-overlay').addClass('with-panel-left');
@@ -134,7 +142,6 @@ $$('.panel-left, .panel-right').on('close', function() {
     $$('.statusbar-overlay').removeClass('with-panel-left with-panel-right');
 });
 
-
 function genChartByHighCharts(_chartData, _elementId, _title, _label_title, _valueSuffix) {
     myApp.hidePreloader();
     $('#' + _elementId).empty();
@@ -144,25 +151,20 @@ function genChartByHighCharts(_chartData, _elementId, _title, _label_title, _val
         }
     });
     $('#' + _elementId).highcharts('StockChart', {
-
         chart: {
             resetZoomButton: {
                 theme: {
                     display: 'none'
                 }
             },
-	        events: {
-	        	load: function() {
-                    if(isPrediction == 0){
-                        this.xAxis[0].setExtremes(
-                            Date.parse(fromdate),
-                            Date.parse(todate)
-                        );
+            events: {
+                load: function() {
+                    if (isPrediction == 0) {
+                        this.xAxis[0].setExtremes(Date.parse(fromdate), Date.parse(todate));
                     }
-	        	}
-	        }
-	    },
-        
+                }
+            }
+        },
         rangeSelector: {
             selected: 2,
             inputEnabled: false,
@@ -191,11 +193,9 @@ function genChartByHighCharts(_chartData, _elementId, _title, _label_title, _val
                 text: 'All'
             }],
         },
-        
         title: {
             text: _title
         },
-
         series: [{
             name: _label_title,
             data: _chartData,
@@ -204,25 +204,19 @@ function genChartByHighCharts(_chartData, _elementId, _title, _label_title, _val
                 valueSuffix: _valueSuffix
             }
         }],
-        
-        
         xAxis: {
-	    	minRange: 6 * 3600 * 1000 // Do not allow user to zoom in less than 6 hours
-	    },
-        
-        
+            minRange: 6 * 3600 * 1000 // Do not allow user to zoom in less than 6 hours
+        },
         exporting: {
             enabled: false
         }
-        
     });
 }
-
-myApp.loadChart = function (firstDate, lastDate) {
-    firstDate = typeof firstDate !== 'undefined' ? firstDate : myApp.getDate(-365);   // Chart data from the first day of current year
-    lastDate  = typeof lastDate !== 'undefined' ? lastDate : myApp.getDate();        // Chart data to today
-    if(isPrediction){
-        if(type == 'duration') {
+myApp.loadChart = function(firstDate, lastDate) {
+    firstDate = typeof firstDate !== 'undefined' ? firstDate : myApp.getDate(-365); // Chart data from the first day of current year
+    lastDate = typeof lastDate !== 'undefined' ? lastDate : myApp.getDate(); // Chart data to today
+    if (isPrediction) {
+        if (type == 'duration') {
             // alert: there is no prediction chart for duration
             myApp.alert("There is no prediction chart for duration");
         } else {
@@ -230,7 +224,7 @@ myApp.loadChart = function (firstDate, lastDate) {
                 genChartByHighCharts(data, 'chart-content', type, type, '');
             });
         }
-    } else if(type == 'duration'){
+    } else if (type == 'duration') {
         $.getJSON(remoteURL + 'type=' + type + '&from=' + firstDate + '&to=' + lastDate).done(function(data) {
             genChartByHighCharts(data, 'chart-content', type, type, ' min');
         });
@@ -240,78 +234,60 @@ myApp.loadChart = function (firstDate, lastDate) {
         });
     }
 };
-
-myApp.updateChart = function () {
+myApp.updateChart = function() {
     var chart = $('#chart-content').highcharts();
-    chart.xAxis[0].setExtremes(
-        Date.parse(fromdate),
-        Date.parse(todate)
-    );
+    chart.xAxis[0].setExtremes(Date.parse(fromdate), Date.parse(todate));
 };
-
-myApp.getDate = function (dayOffSet) {
-    if(dayOffSet == undefined){
+myApp.getDate = function(dayOffSet) {
+    if (dayOffSet == undefined) {
         dayOffSet = 0;
-    }       
+    }
     var today = new Date();
     var milli = (new Date()).getMilliseconds() + dayOffSet * 24 * 3600 * 1000;
     today.setMilliseconds(milli);
     var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+    var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
-
-    if(dd<10) {
-        dd='0'+dd
-    } 
-
-    if(mm<10) {
-        mm='0'+mm
-    } 
-
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
     today = yyyy + '-' + mm + '-' + dd;
     return today;
 };
-
-myApp.updatePeriod = function () {
-    if(isPrediction == 0){
+myApp.updatePeriod = function() {
+    if (isPrediction == 0) {
         var chart = $('#chart-content').highcharts();
         fromdate = myApp.translateDate(chart.xAxis[0].getExtremes().min);
         todate = myApp.translateDate(chart.xAxis[0].getExtremes().max);
     }
 };
-
-myApp.translateDate = function (milli) {
+myApp.translateDate = function(milli) {
     var today = new Date(milli);
     var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+    var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
-
-    if(dd<10) {
-        dd='0'+dd
-    } 
-
-    if(mm<10) {
-        mm='0'+mm
-    } 
-
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
     today = yyyy + '-' + mm + '-' + dd;
     return today;
 };
-
-myApp.myactions = function (params) {
+myApp.myactions = function(params) {
     _modalTemplateTempDiv = document.createElement('div');
-    
     params = params || [];
     if (params.length > 0 && !$.isArray(params[0])) {
         params = [params];
     }
-
     var actionsTemplate = myApp.params.modalActionsTemplate;
     var buttonsHTML = '';
     for (var i = 0; i < params.length; i++) {
         for (var j = 0; j < params[i].length; j++) {
-            
-                
             if (j === 0) buttonsHTML += '<div class="actions-modal-group">';
             var button = params[i][j];
             var buttonClass = button.label ? 'actions-modal-label' : 'actions-modal-button';
@@ -319,38 +295,67 @@ myApp.myactions = function (params) {
             if (button.red) buttonClass += ' actions-modal-button-red';
             if (button.fromdate) {
                 buttonsHTML += '<span class="actions-modal-label"><input type="date" min="2014-05-30" class="list-button item-link from-date" name="from-date" value="' + fromdate + '"></span>';
-            }
-            else if (button.todate) {
+            } else if (button.todate) {
                 buttonsHTML += '<span class="actions-modal-label"><input type="date" min="2014-06-01" class="to-date" name="to-date" value="' + todate + '"></span>';
-            }
-            else if (button.text){
+            } else if (button.text) {
                 buttonsHTML += '<span class="' + buttonClass + '">' + button.text + '</span>';
             }
             if (j === params[i].length - 1) buttonsHTML += '</div>';
         }
     }
-    
     var modalHTML = actionsTemplate.replace(/{{buttons}}/g, buttonsHTML);
-
     _modalTemplateTempDiv.innerHTML = modalHTML;
     var modal = $(_modalTemplateTempDiv).children();
     $('body').append(modal[0]);
-        
     var groups = modal.find('.actions-modal-group');
-    groups.each(function (index, el) {
+    groups.each(function(index, el) {
         var groupIndex = index;
-        $(el).children().each(function (index, el) {
+        $(el).children().each(function(index, el) {
             var buttonIndex = index;
             var buttonParams = params[groupIndex][buttonIndex];
             if ($(el).hasClass('actions-modal-button')) {
-                $(el).on('click', function (e) {
+                $(el).on('click', function(e) {
                     if (buttonParams.close !== false) myApp.closeModal(modal);
                     if (buttonParams.onClick) buttonParams.onClick(modal, e);
                 });
             }
         });
     });
-    
     myApp.openModal(modal);
     return modal[0];
+};
+myApp.login = function(name, password) {
+    $.post(BASE_URL + "login", {
+        name: name,
+        password: password
+    }, function(result) {
+        console.log(result);
+        if (result.status == 200) {
+            $.jStorage.set("token", result.token);
+            // store name and password to local storage
+            $.jStorage.set("name", name);
+            $.jStorage.set("password", password);
+            document.location = 'index.html';
+        } else {
+            myApp.alert('cannot find the user');
+            $.jStorage.deleteKey("name");
+            $.jStorage.deleteKey("password");
+            $('#name').val("");
+            $('#password').val("");
+            return;
+        }
+    });
+};
+myApp.logout = function() {
+    $.ajax({
+        type: 'POST',
+        url: BASE_URL + "logout",
+        async: false
+    });
+    $.jStorage.deleteKey("name");
+    $.jStorage.deleteKey("token");
+    $.jStorage.deleteKey("password");
+    myApp.closePanel();
+    mainView.loadPage('login.html');
+    //document.location = 'index.html';
 };
